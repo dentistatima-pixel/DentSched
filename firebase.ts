8d23ed461736b15a877d2cd82a0b6ab5739f4e5f
dentistatima <dentistatima@gmail.com>
2025-12-03 06:40:30 +0000

    feat: Add Firebase configuration and install SDK

---
+++ firebase.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbrP-4-VvignfNK094mDXNdxqDvfp2ebw",
+  authDomain: "dentsched-9de50.firebaseapp.com",
+  databaseURL: "https://dentsched-9de50-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dentsched-9de50",
+  storageBucket: "dentsched-9de50.firebasestorage.app",
+  messagingSenderId: "1055566993914",
+  appId: "1:1055566993914:web:47fedc4613e62b52b62c43",
+  measurementId: "G-SSWMHV81JY"
+};
